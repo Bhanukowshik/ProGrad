ed5f35d0ddc36f8062f72393786506b06f666261
Harish Paruchuri <31740401+harishparuchuri@users.noreply.github.com>
2020-03-30 13:32:58 +0530

    Add files via upload

---
+++ Java/Day_5/bucket-list/src/Controller/MapServlet.java
@@ -0,0 +1,134 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.TouristPlace;
+import service.*;
+
+
+@WebServlet(urlPatterns= {"/map"})
+public class MapServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+//Create appropriate objects for the map
+	Map<String,TouristPlace> map=new HashMap<String,TouristPlace>();
+	MapOperations mo=new MapOperations();
+	
+	
+	
+	
+
+	@SuppressWarnings("unchecked")
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String destination = request.getParameter("travel");
+		String rank = request.getParameter("rank");
+		String add = request.getParameter("add");
+		
+		String sortRandom = request.getParameter("sortrandomly");
+		String sortInEntryOrder = request.getParameter("sortinentryorder");
+		String sortAlphabetically = request.getParameter("sortalphabetically");
+		String remove = request.getParameter("delete");
+		String reset = request.getParameter("reset");
+		
+		// create object for Toursist class and call the appropriate method
+		TouristPlace tc=new TouristPlace(name,destination,rank);
+		
+		if(add!=null)
+		{  
+			
+			
+			
+			// call the add method and store the return value in a map variable
+		
+			map = mo.add(tc);
+			
+			request.setAttribute("bucketList", map);
+			request.setAttribute("message", "user added successfully");
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		if(remove!=null)
+		{
+			// call the remove method and store the return value in a map variable
+			
+			
+			
+			 
+			map =  (Map<String, TouristPlace>) mo.remove(tc);
+			request.setAttribute("bucketList", map);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		
+		if(sortRandom!=null) 
+		{
+			// call the sortRandomly method and store the return value in a map variable
+		
+			
+			
+			
+			map=(HashMap<String, TouristPlace>) mo.sortRandomly(map);
+			
+			request.setAttribute("bucketList",map);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		if(sortInEntryOrder!=null) {
+			// call the sortInEntryOrder and store the return value in a map variable
+	
+			
+			
+			
+			map=(LinkedHashMap<String, TouristPlace>) mo.sortInEntryOrder(map);
+			
+			request.setAttribute("bucketList",map);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+		if(sortAlphabetically!=null) {
+			MapOperations mo=new MapOperations();
+			
+			
+			
+			map=(TreeMap<String, TouristPlace>) mo.sortRandomly(map);
+			// call the sort Alphabetically and store the return value in a map variable
+			request.setAttribute("bucketList",map);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		if(reset!=null) {	
+			// call the reset method and store the return value in a map variable
+			MapOperations mo=new MapOperations();
+			
+			
+			map=(Map<String, TouristPlace>) mo.reset(map);
+			request.setAttribute("bucketList",map);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
+
---
+++ Java/Day_5/bucket-list/src/Controller/SetServlet.java
@@ -0,0 +1,112 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.TouristPlace;
+import service.ListOperations;
+import service.SetOperations;
+
+@WebServlet(urlPatterns= {"/set"})
+
+public class SetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// Create a set 
+	Set<TouristPlace> set=new HashSet<TouristPlace>();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String destination = request.getParameter("travel");
+		String rank = request.getParameter("rank");
+		String add = request.getParameter("add");
+		
+		String sortbydestination = request.getParameter("sortbydestination");
+		String sortbyrank = request.getParameter("sortbyrank");
+		String remove = request.getParameter("delete");
+		String reset = request.getParameter("reset");
+		
+		// Create an object for TouristPlace
+		// Pass name, destination and rank to the constructor
+		// Create an object for SetOperations and call the appropriate methods 
+		TouristPlace tp = new TouristPlace(name,destination,rank);
+		SetOperations sp=new SetOperations();
+		
+		if(add!=null) 
+		{
+			// call the add method and store the return value in a set variable
+			  set= sp.add(tp);
+			
+			request.setAttribute("bucketListadd",set);
+			request.setAttribute("message", "user added successfully");
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		if(remove!=null) {
+			// call the remove method and store the return value in a set variable
+			 set=sp.remove(tp);
+			
+			
+			request.setAttribute("bucketListremove", set);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		
+		if(sortbydestination!=null)
+		{
+			// call the sortByDestination method and store the return value in a set variable
+			@SuppressWarnings("unchecked")
+			TreeSet<TouristPlace>  se=(TreeSet<TouristPlace>) sp.sortByDestination(set);
+			
+			
+			request.setAttribute("bucketList", se);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		if(sortbyrank!=null) {
+			// call the sortByRank method and store the return value in a set variable
+			@SuppressWarnings("unchecked")
+		TreeSet<TouristPlace> s= (TreeSet<TouristPlace>) sp.sortByRank(set);
+			
+			request.setAttribute("bucketList",s);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+		if(reset!=null) {
+			// call the reset method and store the return value in a set variable
+			@SuppressWarnings("unchecked")
+			   Set<TouristPlace> s =(Set<TouristPlace>) sp.reset(set);
+			
+			request.setAttribute("bucketList",s);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/list.jsp");
+			rd.forward(request, response);
+		}
+
+	
+	
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+
+
+}
